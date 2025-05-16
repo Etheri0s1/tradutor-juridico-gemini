aa4ed5718a04343eacac1654996b74c6d2d45b37
Etheri0s1 <168692995+Etheri0s1@users.noreply.github.com>
2025-05-16 16:13:33 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const GEMINI_CONFIG = {
-  API_KEY: "", 
+  API_KEY: "", // Obtenha em: https://aistudio.google.com/
   API_ENDPOINT_BASE: "https://generativelanguage.googleapis.com/v1/models/",
   MODEL_NAME: "gemini-1.5-flash-latest"
 };
