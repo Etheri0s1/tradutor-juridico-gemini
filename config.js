7170ee92b505bd7546cf12a07c4ffc8dd08594d1
Etheri0s1 <robsonkennedy985@gmail.com>
2025-05-16 15:54:10 -0300

    Remove arquivos sensíveis

--- config.js
+++
@@ -1,5 +0,0 @@
-const GEMINI_CONFIG = {
-    API_KEY: "AIzaSyDH8AQkP0nIKZPKbUv8v1Ezt00oKx1zQUY",
-    API_ENDPOINT_BASE: "https://generativelanguage.googleapis.com/v1beta/models/",
-    MODEL_NAME: "gemini-1.5-flash-latest",
-};
